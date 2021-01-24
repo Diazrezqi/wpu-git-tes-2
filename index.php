1332d87160666cc9f814c81af120ac163a67dbd4
Diazrezqi <diazxrezqi@gmail.com>
2021-01-24 21:10:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,11 @@
 <body>
     <h1>Hello </h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit nobis facere totam cum. Nemo iusto deserunt provident illo! Tempore perferendis odit numquam ratione beatae quod eligendi magni obcaecati laudantium tenetur.</p>
+
+    <ul>
+        <li>666</li>
+        <li>555</li>
+    </ul>
 </body>
 
 </html>
